b7672bcac7d386e2cd1af2bf573dd6166c8c56da
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-05-29 16:43:16 +0000

    primittive datatypes

---
+++ Veriables/datatypes.java
@@ -0,0 +1,19 @@
+public class datatypes{
+    public static void main(String args[]){
+        byte b = 8;
+        char a = 'a';
+        boolean var = true;
+        float price = 10.5f;
+        int num = 10;
+        //long 
+        //double
+        short n = 250;
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(var);
+        System.out.println(price);
+        System.out.println(num);
+        System.out.println(n);
+        // System.out.println();
+    }
+}
