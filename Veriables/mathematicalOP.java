75441904a353b41737abb3a39633db7ffa7fa1f0
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-05-29 17:05:29 +0000

    basic math operations

---
+++ Veriables/mathematicalOP.java
@@ -0,0 +1,14 @@
+public class mathematicalOP{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        int mult = a * b;
+        int div = b/a;
+        int sub = b - a;
+        System.out.println(sum);
+        System.out.println(mult);
+        System.out.println(div);
+        System.out.println(sub);
+    }
+}
