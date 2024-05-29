25779015bdef51aa2e5d9ec9c330ea66a366e89c
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-05-29 16:47:02 +0000

    datatypes

--- Veriables/datatypes.java
+++ Veriables/datatypes.java
@@ -1,13 +1,13 @@
 public class datatypes{
     public static void main(String args[]){
-        byte b = 8;
-        char a = 'a';
-        boolean var = true;
-        float price = 10.5f;
-        int num = 10;
-        //long 
-        //double
-        short n = 250;
+        byte b = 8; //1byte
+        char a = 'a'; // 2byte
+        boolean var = true; //1byte
+        float price = 10.5f; //4byte
+        int num = 10; //4byte
+        //long 8byte
+        //double 8bytes
+        short n = 250; //2byte
         System.out.println(b);
         System.out.println(a);
         System.out.println(var);
