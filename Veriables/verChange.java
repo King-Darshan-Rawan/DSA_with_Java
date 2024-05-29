2fa91a6001552c83708a4ab4b0f84646a33c735c
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-05-29 16:31:36 +0000

    play with veriables

---
+++ Veriables/verChange.java
@@ -0,0 +1,12 @@
+public class verChange{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        System.out.println("a");
+        System.out.println(a);
+        System.out.println(b);
+
+        a = 50;
+        System.out.println(a);
+    }
+}
