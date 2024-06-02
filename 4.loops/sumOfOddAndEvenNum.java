f09ec73810f96ad3dbf0f16d52511e1224c64d66
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-06-02 16:26:05 +0000

    sum of odd and even numbers in a range of inigers

---
+++ 4.loops/sumOfOddAndEvenNum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class sumOfOddAndEvenNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a range");
+        System.out.print("Start = ");
+        int startRange = sc.nextInt(); 
+        System.out.print("End = ");
+        int endRange = sc.nextInt();
+        int sumOdd = 0;
+        int sumEven = 0;
+        for (int i = startRange; i <= endRange; i++) {
+            if (i % 2 == 0) {
+                sumEven = sumEven + i;
+            }
+            else{
+                sumOdd = sumOdd + i;
+            }
+        }
+        System.out.println("The sum of even numbers = " + sumEven);
+        System.out.println("The sum of odd numbers = " + sumOdd);
+    }
+}
