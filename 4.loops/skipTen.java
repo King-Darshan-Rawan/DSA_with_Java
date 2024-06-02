35dcbcba978a7debf39ebe58602b72cd9fe1dd52
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-06-02 15:37:24 +0000

    skip the multiple of 10

---
+++ 4.loops/skipTen.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class skipTen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers ");
+        do {
+            int n = sc.nextInt();
+            if (n % 10 == 0) {
+                System.out.println("skip multiple of 10");
+                continue;
+            }
+            System.out.println(" " + n);
+        } while (true);
+        
+    }
+}
