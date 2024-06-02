9f61fe9733ed0c770acaf769698ecc8f55bd6c4a
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-06-02 15:21:18 +0000

    Reverse the number

--- 4.loops/reverse.java
+++ 4.loops/reverse.java
@@ -1,12 +1,16 @@
 import java.util.*;
 
 public class reverse {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter a number = ");
         int n = sc.nextInt();
+        /* 
         int num = n;
         int count = 0;
+
+        // normal logic
+        
         while (num != 0) {
             num = num/10;
             count++;
@@ -17,6 +21,18 @@ public class reverse {
             n = n / 10;
         }
         System.out.println();
+        */
+        
+        // logic ++
+
+        int rev = 0;
+        int rem;
+        while (n > 0) {
+            rem = n % 10;
+            rev = (rev * 10) + rem; 
+            n = n / 10;
+        }
+        System.out.println("the reverse is = " + rev);
         sc.close();
     }
 }
