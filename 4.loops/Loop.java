0052b087db0d137b998f49257a771dc3574cb5e2
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-06-02 04:16:54 +0000

    while loop test

---
+++ 4.loops/Loop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        while (count < 11) {
+            System.out.println(count);
+            count ++;
+        }
+
+        sc.close();
+    }
+}
