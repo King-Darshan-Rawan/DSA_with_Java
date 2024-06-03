cb17bcf5eef019f3d0ffb0677c69495b60acca63
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-06-03 13:21:17 +0000

    number Pyramid

--- 6.AdvancePatterns/numberpyramid.java
+++ 6.AdvancePatterns/numberpyramid.java
@@ -10,7 +10,7 @@ public class numberpyramid {
                 System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
-                System.out.print(" " + i +" ");
+                System.out.print( i +" ");
             }
             System.out.println();
         }
