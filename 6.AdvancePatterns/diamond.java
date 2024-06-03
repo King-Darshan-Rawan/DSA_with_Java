1f5199264a49e4ac8b0e94e7962b05848d4cb53f
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-06-03 13:05:02 +0000

    diamond pattern

---
+++ 6.AdvancePatterns/diamond.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class diamond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of a side = ");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <=n; i++) {
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = n - 1; j > i; j--) {
+                System.out.print("*");
+            }
+            for (int j = n; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
