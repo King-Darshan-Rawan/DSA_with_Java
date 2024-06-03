cce3a169b4520b5d4fe07e4a1dcaa2c74b51958b
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-06-03 04:05:28 +0000

    triangle pattern

--- 5.Patterns/trianglePattern.java
+++ 5.Patterns/trianglePattern.java
@@ -4,12 +4,21 @@ public class trianglePattern {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter the side = ");
         int n = sc.nextInt();
+        // lowerbottom
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print(" * ");    
             }
             System.out.println();
         }    
+        System.out.println("\n-----triangle-----\n");
+        // upper bottom
+        for (int i = n; i > 0; i--) {
+            for (int j = i; j > 0 ; j--) {
+                System.out.print(" * ");    
+            }
+            System.out.println();
+        } 
         sc.close();
     }
 }
