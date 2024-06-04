a85a1441c52bb2e69ea50f2fc7b8230821fe1b7d
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-06-04 10:41:54 +0000

    Sum of integers Function

---
+++ 7.Functions/sumFunction.java
@@ -0,0 +1,11 @@
+public class sumFunction {
+    public static int sumTwo(int n, int m){ //paramiters
+        int sum = n + m;
+        return sum;
+    }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        System.out.println(sumTwo(a, b)); // a,b => arguments
+    }
+}
