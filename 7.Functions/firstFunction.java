ca6fd5782a77a15e50fa71586dcd49f357c24f71
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-06-04 10:26:58 +0000

    Function used first time

---
+++ 7.Functions/firstFunction.java
@@ -0,0 +1,8 @@
+public class firstFunction {
+    public static void printWorld(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printWorld();
+    }
+}
