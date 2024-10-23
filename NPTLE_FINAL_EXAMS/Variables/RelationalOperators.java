11f92fc1e1ad1ef907f07a43d58d76f69e033a68
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-10-23 03:03:00 +0000

    ro

---
+++ NPTLE_FINAL_EXAMS/Variables/RelationalOperators.java
@@ -0,0 +1,12 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        System.out.println("Are equal to " + (a == b));
+        System.out.println("Are not equal to " + (a != b));
+        System.out.println("Are a is grator to " + (a > b));
+        System.out.println("Are b is grator to " + (a < b));
+        System.out.println("Are a is grator then equal to " + (a >= b));
+        System.out.println("Are a is less then equal to " + (a <= b));
+    }
+}
