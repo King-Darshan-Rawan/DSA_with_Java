c06f632459968e79cc0320e9ff85977dbff2b523
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-10-23 03:03:17 +0000

    sum java

---
+++ NPTLE_FINAL_EXAMS/Variables/sumOfAandB.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sumOfAandB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers A & B Respectively");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
