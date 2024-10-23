f64b52f350f8265ead510a3b4245d4e9acacad4e
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-10-23 03:02:51 +0000

    ar

---
+++ NPTLE_FINAL_EXAMS/Variables/AreathamaticOperators.java
@@ -0,0 +1,16 @@
+public class AreathamaticOperators {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 3;
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Remaindor = " + (a / b));
+        int c = ++a;
+        System.out.println("a = " + c);
+        int d = a++;
+        System.out.println("a = " + d);
+
+    }
+}
