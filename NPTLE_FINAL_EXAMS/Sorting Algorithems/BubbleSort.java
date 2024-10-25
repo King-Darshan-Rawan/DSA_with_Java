25df6afa3b725c24bc7bd6919379060f138a4f72
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-10-25 09:24:18 +0000

    Bubble Short

--- NPTLE_FINAL_EXAMS/Sorting Algorithems/BubbleSort.java
+++ NPTLE_FINAL_EXAMS/Sorting Algorithems/BubbleSort.java
@@ -1,20 +1,25 @@
+
 public class BubbleSort {
-    public static void BubblelyArr(int [] arr){
-        for(int turns = 0; turns <= arr.length - 2; turns ++){
-            for(int i = 0; i <= arr.length - 2 - turns; i++){
-                if(arr[i] > arr[i + 1]){int temp = arr[i];
-                arr[i]= arr[i+1];
-                arr [i+1] = temp;}
+
+    public static void BubblelyArr(int[] arr) {
+        for (int turns = 0; turns <= arr.length - 2; turns++) {
+            for (int i = 0; i <= arr.length - 2 - turns; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
             }
         }
         for (int i = 0; i < arr.length; i++) {
-         
-        System.out.print(arr[i] + " ");   
+
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
-    public static void main(String [] args){
-        int [] arr = {1,5,7,3,4,0};
+
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 3, 4, 0};
         BubblelyArr(arr);
-    }    
+    }
 }
