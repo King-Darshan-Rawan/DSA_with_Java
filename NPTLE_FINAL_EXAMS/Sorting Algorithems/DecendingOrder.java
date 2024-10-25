c5e41b86e0d7b30a5e9746e8f336b703a180d97f
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-10-25 09:24:30 +0000

    Decending

---
+++ NPTLE_FINAL_EXAMS/Sorting Algorithems/DecendingOrder.java
@@ -0,0 +1,13 @@
+public class DecendingOrder {
+    public static void BubleSort(int [] arr){
+        for(int i =0; i < arr.length; i++){
+            for (int idx = 0; idx < arr.length; idx++) {
+                
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr = {3,6,2,1,8,7,4,5,3,1};
+
+    }
+}
