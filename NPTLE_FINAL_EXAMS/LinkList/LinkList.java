7f9d7d9dd7a871a90cc5471fa9ff2245bba5a691
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-10-26 03:53:21 +0000

    linklist

---
+++ NPTLE_FINAL_EXAMS/LinkList/LinkList.java
@@ -0,0 +1,61 @@
+public class LinkList {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print(){
+        if(head == null){
+            System.out.println("LL is empty");
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkList list = new LinkList();
+        list.print();
+        list.addFirst(1);
+        list.print();
+
+        list.addFirst(2);
+        list.print();
+
+        list.addLast(3);
+        list.print();
+
+        list.addLast(4);
+        list.print();
+        // System.out.println(list);
+    }
+}
