3f6a8a3587585eaa1e41614ab88002fb5ac17a25
Darshan Malviya <147977033+King-Darshan-Rawan@users.noreply.github.com>
2024-10-26 05:30:07 +0000

    remove first

--- NPTLE_FINAL_EXAMS/LinkList/LinkList.java
+++ NPTLE_FINAL_EXAMS/LinkList/LinkList.java
@@ -1,3 +1,6 @@
+
+import javax.swing.LayoutStyle;
+
 public class LinkList {
     public static class Node{
         int data;
@@ -9,9 +12,11 @@ public class LinkList {
     }
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addFirst(int data){
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -19,15 +24,41 @@ public class LinkList {
         newNode.next = head;
         head = newNode;
     }
+    public void removeFirst(){
+        size--;
+        Node temp = head;
+        if(head == null){
+            head = tail = null;
+            return;
+        }
+        head = head.next;
+    }
+    
 
     public void addLast(int data){
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
         }
         tail.next = newNode;
         tail = newNode;
+    }
+    public void removeLast(){
+        
+        size--;
+        if(head == null){
+            head = tail = null;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != tail){
+            temp = temp.next;
+            
+        }
+        temp.next = null;
+        tail = head;
     }
 
     public void print(){
@@ -40,6 +71,20 @@ public class LinkList {
             temp = temp.next;
         }
         System.out.println("null");
+    }
+
+    public void add(int idx, int data){
+        Node newNode = new Node(data);
+        Node temp = head;
+        size++;
+        int i = 0 ;
+        while( i < idx - 1){
+            temp = temp.next;
+            i++;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
 
     public static void main(String[] args) {
@@ -56,6 +101,18 @@ public class LinkList {
 
         list.addLast(4);
         list.print();
+
+        list.add(2,7);
+        list.print();
         // System.out.println(list);
+        System.out.println(list.size);
+        list.print();
+        list.removeLast();
+        list.print();
+        System.out.println(list.size);
+        list.removeFirst();
+        list.print();
+        System.out.println(list.size);
+
     }
 }
